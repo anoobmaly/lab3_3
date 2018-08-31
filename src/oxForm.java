0c5d954e611c510da0e0c4829ea16b81cc578b04
supisara1511 <supisara_2540@hotmail.com>
2018-08-31 09:58:12 +0700

    Inprogress

--- src/oxForm.java
+++ src/oxForm.java
@@ -1,2 +1,18 @@
+import javax.swing.*;
+
 public class oxForm {
+    private JLabel scoreX;
+    private JLabel scoreDraw;
+    private JPanel JPanel;
+    private JPanel mainPanel;
+    private JLabel scoreO;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
 }
